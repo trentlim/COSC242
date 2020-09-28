7d9fb2495b349232b4f28fa27f3b9f2f4432788a
trentlim <trent_lim_nz@yahoo.com>
2020-09-29 12:08:40 +1300

    Update bst.c

--- lab22b/bst.c
+++ lab22b/bst.c
@@ -4,14 +4,51 @@
 #include "mylib.h"
 #include "bst.h"
 
-struct bst_node{
+struct bst_node {
     char *key;
     bst left;
     bst right;
 };
 
-void bst_inorder(bst b, void f(char *key)){
-    if (b == NULL){
+bst bst_new() {
+    return NULL;
+}
+
+bst bst_insert(bst b, char *str){
+    if (b == NULL) {
+        b = emalloc(sizeof *b);
+        b->key = emalloc((strlen(str) + 1) * sizeof b->key[0]);
+        strcpy(b->key, str);
+        b->left = NULL;
+        b->right = NULL;
+        return b;
+    }
+    if (strcmp(b->key, str) > 0) {
+         b->left = bst_insert(b->left, str);
+    }else{
+         b->right = bst_insert(b->right, str);
+    }
+    return b;
+}
+
+int bst_search(bst b, char *str){
+    if (b == NULL) {
+        return 0;
+    }
+    if (strcmp(b->key, str) == 0) {
+        return 1;
+    }else{
+        if (strcmp(b->key, str) > 0) {
+            return bst_search(b->left, str);
+        }else{
+            return bst_search(b->right, str);
+        }
+    }
+    return 0;
+}
+
+void bst_inorder(bst b, void f(char *str)){
+    if (b == NULL) {
         return;
     }
     bst_inorder(b->left, f);
@@ -19,52 +56,23 @@ void bst_inorder(bst b, void f(char *key)){
     bst_inorder(b->right, f);
 }
 
-bst bst_insert(bst b, char *key){
-    if (b == NULL){
-        b = emalloc(sizeof *b);
-        b->key = emalloc(strlen(key)+1);
-        strcpy(b->key, key);
-    } else if (strcmp(b->key, key) < 0){
-        b->right = bst_insert(b->right, key);
-    } else if (strcmp(b->key, key) > 0){
-        b->left = bst_insert(b->left, key);
-    }
-    return b;
-}
-
-bst bst_new(){
-    return NULL;
-}
-
-void bst_preorder(bst b, void f(char *key)){
-    if (b == NULL){
+void bst_preorder(bst b, void f(char *str)){
+    if (b == NULL) {
         return;
     }
+
     f(b->key);
     bst_preorder(b->left, f);
     bst_preorder(b->right, f);
 }
 
-int bst_search(bst b, char *key){
-    if (b == NULL){
-        return 0;
-    }
-    if (strcmp(b->key, key) == 0){
-        return 1;
-    } else if (strcmp(b->key, key) < 0){
-        return bst_search(b->right, key);
-    } else {
-        return bst_search(b->left, key);
-    }
-    return 0;
-}
-
 bst bst_free(bst b){
-    if (b == NULL){
-        return b;
+    if (b == NULL) {
+        return NULL;
     }
-    b->right = bst_free(b->right);
-    b->left = bst_free(b->left);
+
+    bst_free(b->left);
+    bst_free(b->right);
     free(b->key);
     free(b);
     return b;
